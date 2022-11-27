175acf1df21971caf9957d2176d26b9bf4979d54
Arnaldo Espinoza <arnaldoespinoza1@gmail.com>
2022-11-27 18:17:20 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='logo de freecodecamp'
+        />
+        <div className='contenedor-calculadora'>
+
+        </div>
+
+
+      </div>
     </div>
   );
 }
