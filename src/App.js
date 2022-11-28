31d7ab6b16ddced8d10b2292730ad2cabdb4257c
Arnaldo Espinoza <arnaldoespinoza1@gmail.com>
2022-11-27 20:17:59 -0400

    siguente commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/logo.png';
+import Boton from './components/Boton';
 
 function App() {
   return (
     <div className="App">
-      <div className='freecodecamp-logo-contenedor'>
-        <img
-          src={freeCodeCampLogo}
-          className='freecodecamp-logo'
-          alt='logo de freecodecamp'
-        />
-        <div className='contenedor-calculadora'>
+            <div className='freecode-camp-logo-contenedor'>
+              <img
+                src={freeCodeCampLogo}
+                className='freecodecamp-logo'
+                alt='logo de freecodecamp' />
+            </div>
 
-        </div>
+            <div className='contenedor-calculadora'>
+                <div className='fila'>
+                <Boton>1</Boton>
+                <Boton>2</Boton>
+                <Boton>3</Boton>
+                <Boton>+</Boton>
+                </div>
+                
+                <div className='fila'>
+                <Boton>4</Boton>
+                <Boton>5</Boton>
+                <Boton>6</Boton>
+                <Boton>-</Boton>
+                </div>
+                
+               
+                <div className='fila'>
+                <Boton>7</Boton>
+                <Boton>8</Boton>
+                <Boton>9</Boton>
+                <Boton>*</Boton>
 
-
-      </div>
+                </div>
+                <div className='fila'>
+                <Boton>=</Boton>
+                <Boton>0</Boton>
+                <Boton>.</Boton>
+                <Boton>/</Boton>
+                </div>
+                <div className='fila'></div>
+            </div>
     </div>
   );
 }
---
+++ src/components/Boton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  '../styles/boton.css';
+
+function Boton(props) {
+  const esOperador=valor=>{
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  }
+    return (
+          <div className={`boton-contendor ${esOperador(props.children)? 'operador' : ''} `.trimEnd()} onClick={``}>
+            {props.children}
+          </div>
+    );
+}
+
+export default Boton;
